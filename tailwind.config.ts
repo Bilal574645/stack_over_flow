487c4e83784e4b2051a5ad6912dd12d33b610e4e
Bilal574645 <bilal441465@gmail.com>
2024-07-24 14:53:02 +0500

    implement tailwind its config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Space_Grotesk } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -7,14 +8,39 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          500: "#FF7000",
+          100: "#FFF1E6",
+        },
+        dark: {
+          100: "#000000",
+          200: "#0F1117",
+        },
+        light: {
+          900: "#FFFFFF",
+          800: "#F4F6F8",
+          850: "#FDFDFD",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
+        },
+        "accent-blue": "#1DA1F2",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        SpaceGrotesk: ["var(--font-spaceGrotesk)"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
